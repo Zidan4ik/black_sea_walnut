4d0c635d2354d7d5090b1d01d71dc72ef339f395
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2024-11-27 22:04:05 +0200

    Initial commit

---
+++ src/main/java/org/example/black_sea_walnut/BlackSeaWalnutApplication.java
@@ -0,0 +1,13 @@
+package org.example.black_sea_walnut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackSeaWalnutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackSeaWalnutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/black_sea_walnut/BlackSeaWalnutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.black_sea_walnut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackSeaWalnutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
