236b74e30dd13a4f3df7e23d7d02613f628a0c8b
Zidan4ik <roman.pravnyk.tr.2021@lpnu.ua>
2025-03-01 00:31:08 +0200

    Added enum,fix structure html in navbar, cart and account, pages about authorization

--- src/main/java/org/example/black_sea_walnut/controller/user/AuthorizationController.java
+++ src/main/java/org/example/black_sea_walnut/controller/user/AuthorizationController.java
@@ -1,4 +1,38 @@
 package org.example.black_sea_walnut.controller.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/web")
 public class AuthorizationController {
+
+    @GetMapping("/login")
+    public ModelAndView viewRegistration() {
+        return new ModelAndView("web/authorize/login");
+    }
+
+    @GetMapping("/registration")
+    public ModelAndView viewRegistration2() {
+        return new ModelAndView("web/authorize/registration");
+    }
+
+    @GetMapping("/terms-of-use")
+    public ModelAndView viewTermsOfUse() {
+        return new ModelAndView("web/terms-of-use");
+    }
+
+    @GetMapping("/thanks")
+    public ModelAndView viewThanks() {
+        return new ModelAndView("web/thanks");
+    }
+
+    @GetMapping("/password-recovery")
+    public ModelAndView viewPasswordRecovery() {
+        return new ModelAndView("web/password-recovery");
+    }
 }
